9c484af8479238d2cccd5b1d44ba3ff9149994f4
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-06-09 19:46:33 +0300

    submits orm lab

--- lib/employee.py
+++ lib/employee.py
@@ -2,6 +2,7 @@
 from __init__ import CURSOR, CONN
 from department import Department
 
+
 class Employee:
 
     # Dictionary of objects saved to the database.
@@ -18,6 +19,8 @@ class Employee:
             f"<Employee {self.id}: {self.name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
+    
+    
 
     @property
     def name(self):
@@ -135,15 +138,13 @@ class Employee:
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
 
-        # Check the dictionary for  existing instance using the row's primary key
+        
         employee = cls.all.get(row[0])
         if employee:
-            # ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
@@ -187,4 +188,9 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        sql ="""
+            SELECT * FROM reviews WHERE employee_id = ?
+        """
+        from review import Review
+        rows = CURSOR.execute(sql, (self.id,)).fetchall()
+        return [Review.instance_from_db(row) for row in rows] if rows else []
